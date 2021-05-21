dcecdd10e048d7e455c934544537078a02fc6ffe
dougchristie70 <>
2021-05-21 01:36:35 +0000

    added the function

--- hello.js
+++ hello.js
@@ -1,1 +1,6 @@
-console.log("Hello world!");
+function hello() {
+	// TODO: change this string so that your program prints "Hello world!" 
+	// when you hit the replit run button
+	return "Hello world!"; 
+}
+module.exports = hello;
